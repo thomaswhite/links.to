a7152a5d5a189087add5368140f97c60a6db3804
Thomas White <thomas.0007@gmail.com>
2013-04-29 22:45:55 +0100

    Checking in some stashed files

---
+++ ss/index.js
@@ -0,0 +1,43 @@
+
+/*
+ * GET home page.
+ */
+
+var box = require('../box.js')
+    , _ = require('lodash')
+    , breadcrumbs = require('./breadcrumbs.js')
+    , config
+    , app
+
+    ;
+
+require('./collections');
+require('./links');
+
+
+
+function top ( req, res ){
+    res.render('layout', { title: '' });
+}
+
+box.on('init', function (App, Config, done) {
+    app = App;
+    config = Config;
+    done(null, 'routers index.js initialised');
+});
+
+box.on('atach-paths', function (app, config,  done) {
+    app.use(
+        box.middler()
+         .get('/', top)
+         .handler
+    );
+    done(null, 'routes index attached'  );
+});
+
+
+exports.init = function( App, Config ){
+    config = Config;
+    app = App;
+    return this;
+};
