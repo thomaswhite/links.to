f9434efb657944927c0cec9a44de03d42c2dd25e
Thomas White <thomas.0007@gmail.com>
2013-05-09 14:05:11 +0100

    socket.io session is working

--- plugins/socket-io.js
+++ plugins/socket-io.js
@@ -1,42 +1,46 @@
 var box = require('../box.js')
-   , SessionSockets = require('session.socket.io')
+  , SessionSockets = require('session.socket.io')
   , MongoStore = require('socket.io-mongo')
   , io
-  , config;
+  , config
+  , store
+  ;
 
 
 box.on('init.listen', function(done) {
-    box.io = io = require('socket.io').listen(box.server);
-    box.sessionSockets = new SessionSockets(io,  box.sessionStore, box.cookieParser);
-    box.emit('init.socket-io', io, box.sessionSockets );
-    /*
-     io.configure(function() {
+     box.io = io = require('socket.io').listen(box.server);
+
      var store = new MongoStore({
-     collectionPrefix: 'socket.io.',
-     streamCollection: 'stream',
-     storageCollection: 'storage',
-     nodeId: null,  // id that uniquely identifies this node
-     size: 1000000, // max size in bytes for capped collection
-     num: null,     // max number of documents inside of capped collection
-     // url: 'mongodb://localhost:27017/yourdb',
-     host: 'localhost',
-     port: 27017,
-     db: 'socketio'
+         collectionPrefix: 'socket.io.',
+         streamCollection: 'stream',
+         storageCollection: 'storage',
+         nodeId: null,  // id that uniquely identifies this node
+         size: 1000000, // max size in bytes for capped collection
+         num: null,     // max number of documents inside of capped collection
+         // url: 'mongodb://localhost:27017/yourdb',
+         host: 'localhost',
+         port: 27017,
+         db: 'socketio'
      });
      store.on('error', console.error);
-     io.set('store', store);
-     });
-     */
+//     io.set('store', store);
+
+
+    box.sessionSockets = new SessionSockets(io,  box.sessionStore, box.cookieParser);
+    box.emit('init.socket-io', io, box.sessionSockets );
     done(null, 'plugin socket.io initialised');
 });
 
-/*
  box.on('init', function (App, Config, done) {
      config = Config;
- done(null, 'plugin socket.io initialised');
+     store = new MongoStore({url: 'mongodb://localhost:27017/yourdb'});
+     store.on('error', console.error);
+
+     done(null, 'plugin socket.io initialised');
  });
 
 
+/*
 box.on('init.attach', function (app, config, done) {
 });
 box.on('init.server', function (server) {
--- socket-io.js
+++ socket-io.js
@@ -29,3 +29,12 @@ box.on('init.socket-io', function (io, sessionSockets) {
 
 });
 
+/*
+ io.sockets.on('connection', function(socket) {}) - initial connection from a client. socket argument should be used in further communication with the client.
+ socket.on('message', function(message, callback) {}) - "message" is emitted when a message sent with socket.send is received. message is the message sent, and callback is an optional acknowledgement function.
+ socket.on('anything', function(data) {}) - "anything" can be any event except for the reserved ones.
+ socket.on('disconnect', function() {}) - the disconnect event is fired in all cases, when the client-server connection is closed.
+           It fires on wanted, unwanted, mobile, unmobile, client and server disconnects.
+           There is no dedicated reconnect event. You have to use the "connection" event for reconnect handling.
+ */
+
