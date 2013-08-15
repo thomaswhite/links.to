6e3f100d1047fdac53cabbaee2f03c1f15a94f44
Thomas White <thomas.0007@gmail.com>
2013-08-16 00:13:31 +0100

    Parsing uploaded bookmarks in progress.

--- public/js/_example_iframe-post-form.js
+++ public/js/_example_iframe-post-form.js
@@ -1,104 +1,16 @@
-$(function ()
-{
-	$('#content form').iframePostForm
-	({
+$(function (){
+	$('form').iframePostForm({
 		json : true,
-		post : function ()
-		{
-			var message;
-			
-			if (!$('.message').length)
-			{
-				$('#demonstrations').after('<div class="message" style="display:none; padding:10px; text-align:center" />');
-			}
-			
-			
-			if ($('input[type=file]').val().length)
-			{
-				$('.message')
-					.html('Uploading file&hellip;')
-					.css({
-						color : '#006100',
-						background : '#c6efce',
-						border : '2px solid #006100'
-					})
-					.slideDown();
-			}
-			
-			else
-			{
-				$('.message')
-					.html('Please select an image for uploading.')
-					.css({
-						color : '#9c0006',
-						background : '#ffc7ce',
-						border : '2px solid #9c0006'
-					})
-					.slideDown();
-				
-				return false;
-			}
+		post : function ()	{
+			console.info('Uploading..');
 		},
-		complete : function (response)
-		{
-			var style,
-				width,
-				html = '';
-			
-			
-			if (!response.success)
-			{
-				$('.message').slideUp(function ()
-				{
-					$(this)
-						.html('There was a problem with the image you uploaded')
-						.css({
-							color : '#9c0006',
-							background : '#ffc7ce',
-							borderColor : '#9c0006'
-						})
-						.slideDown();
-				});
+		complete : function (response){
+
+			if (!response.success){
+                console.error('Bad upload');
+			}else{
+                console.error('Upload OK');
 			}
-			
-			else
-			{
-				html += '<p>Below is the uploaded image and the values that were posted when you submitted the demonstration form.</p>';
-				
-				
-				if (response.postedValues)
-				{
-					for (title in response.postedValues)
-					{
-						html += '<strong>' + title + ':</strong> ' + response.postedValues[title] + '<br />';
-					}
-				}
-				
-				
-				if (response.imageSize)
-				{
-					width = response.imageSize[0] > 500 ? 500 : response.imageSize[0];
-				}
-				
-				
-				if (response.imageSource)
-				{
-					html += '<img src="' + response.imageSource + '" width="' + width + '" alt="Your uploaded image" />';
-				}
-				
-				
-				$('.message').slideUp(function ()
-				{
-					$(this)
-						.html(html)
-						.css({
-							color : '#006100',
-							background : '#c6efce',
-							borderColor : '#006100'
-						})
-						.slideDown();
-				});
-			}
-		}
+        }
 	});
 });
--- routes/upload.js
+++ routes/upload.js
@@ -8,22 +8,46 @@
 
 
 var box = require('../box.js')
-    , formidable = require('formidable')
     ,  util = require('util')
     , debug = require('debug')('linksTo:view.collections')
+    , fs = require('fs')
+    , cheerio = require('cheerio')
     , config
     , app
 
     ;
 
 
+var cherioParam = {
+    ignoreWhitespace: false,
+    xmlMode: true,
+    lowerCaseTags: true
+};
+
+
+function deleteAfterUpload (path) {
+    setTimeout( function(){
+        fs.unlink(path, function(err) {
+            if (err) console.log(err);
+            console.log('file successfully deleted:' + path);
+        });
+    }, 10 * 1000);
+}
+
+function getAttr_and_Name( $s, dest ){
+    dest.name = $s.text();
+    for( var i in $s.attribs){
+        dest[i] = $s.attribs[i];
+    }
+    return dest;
+}
+
+
 function upload (req, res) {
+/*
     var form = new formidable.IncomingForm();
     form.keepExtensions = true;
     form.uploadDir =  config.__dirname + config.upload.dir;
-
-
-
 
     form.parse(req, function(err, fields, files){
         if (err) return res.end('You found error');
@@ -32,7 +56,7 @@ function upload (req, res) {
         console.log("parse:\n" + util.inspect( fields, false, 7, true ) + "\n" + util.inspect( files, false, 7, true )  );
     });
 
-/*
+
     form.progress( function(bytesReceived, bytesExpected){
         var percent = (bytesReceived / bytesExpected * 100) | 0;
         process.stdout.write('Uploading: %' + percent + '\r');
@@ -45,7 +69,7 @@ function upload (req, res) {
  res.end('error:\n\n'+util.inspect(err));
  });
 
-*/
+
     req.form.complete(function(err, fields, files){
         if (err) {
             next(err);
@@ -56,12 +80,68 @@ function upload (req, res) {
             res.redirect('back');
         }
     });
+*/
 
-    // console.log("uploaded:\n" + util.inspect(  req.files.inputTypeFile , false, 7, true )  );
+  //  console.log("uploaded:\n" + util.inspect(  req.files.favorites , false, 7, true )  );
 
+
+    fs.readFile(req.files.favorites.path, function (err, data) {
+        var s = '' + data;
+ //       console.info( s );
+
+        s = s.replace(/<p>|<HR>|ICON=".*"|ICON_URI=".*"/gi, '');
+        s = s.replace(/<\/A>/gi, '</a></dt>');
+        s = s.replace(/<\/h3>/gi, '</h3></dt>');
+
+//        if( /<dd>.^[<](<dl>)/.test(s) )
+
+        s = s.substring( s.indexOf( '<DL>' ) - 1);
+ //       console.info( s );
+
+        var result,
+            $ = cheerio.load( '<body>' + s + '</body>', cherioParam);
+
+        console.info($.html() );
+
+       result = parseDL( $(' body > dl' ), $ );
+       console.log("parse:\n" + util.inspect( result, false, 7, true ));
+
+
+    });
+
+    deleteAfterUpload( req.files.favorites.path );
     res.end('Done');
     return;
-};
+}
+
+
+
+function parseDL( $DL, $ ){
+    return $DL.children('>DT').map(function(pos, element){
+        var $DT = $(this),
+            $A  = $DT.children('>a'),
+            $next = $DT.next ? $DT.next() : null,
+            step = {
+                children:[]
+            };
+        if( !$next ){
+            var dummy;
+        }else{
+            if( $next.is('h3')){      // folder
+                var $dd = $next ? $next.next() : null;
+                if( $dd.is('dd')){
+                    step.description = $next_next.text();
+                }
+                getAttr_and_Name( $next, $ );
+                step.children = parseDL($DT.children('>DL'));
+            }else if( $A.is('a')){ // link
+                getAttr_and_Name( $next, step );
+            }
+        }
+
+        return step;
+    });
+}
 
 
 box.on('init', function (App, Config, done) {
