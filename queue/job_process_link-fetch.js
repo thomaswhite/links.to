db5cecc4f011263a18fa13467b6319f537bb4519
Thomas White <thomas.0007@gmail.com>
2013-10-24 22:45:06 +0100

    Refactor pageScraper II

--- queue/job_process_link-fetch.js
+++ queue/job_process_link-fetch.js
@@ -13,17 +13,8 @@ var job_id = 'link-fetch'
     , request = require('request')
     , _ = require('lodash')
     , linkDisplay = require('../lib/link-make-display')
-    , cheerio = require('cheerio')
     , config
 ;
-
-
-var cherioParam = {
-    ignoreWhitespace: false,
-    xmlMode: true,
-    lowerCaseTags: true
-};
-
 
 
 function make_link_display( oURL, oURL2){
